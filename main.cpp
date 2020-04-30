8a8256e77749b391e9420152e164d6041c1189c3
mperry37 <mperry37@alaska.edu>
2020-04-29 23:49:38 -0800

    feat: chain of command working

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@
 #include <vector>
 
 
-void clientRequests(HelpDesk & helper){
+void clientRequests(std::shared_ptr<HelpDesk> helper){
     std::vector<std::string> commands = {"Accounting", "Payments", "Repairs"};
     for(const std::string & c : commands) {
         std::cout << "HelpDesk request to: " << c << "!\n";
-        const std::string result = helper.Help(c); 
+        const std::string result = helper->Help(c); 
         if (!result.empty()){
             std::cout << " " << result;
+        }
+        else{
+            std::cout << "Not Availble to Process \n";
         }
     }
 }
@@ -19,9 +22,12 @@ int main(){
     auto billing = std::make_shared<PaymentsHelper>();
     auto repairs = std::make_shared<RepairHelper>();
     accounting->setNext(billing)->setNext(repairs);
-
+    std::cout << accounting->Help("Billing") << "\n";
     std::cout << "Chain of command: Acccounting > Payments > Repairs \n\n";
     clientRequests(accounting);
+
+    std::cout << "Chain starting at Payments > Repairs \n";
+    clientRequests(billing);
     return 0;
 }
 
